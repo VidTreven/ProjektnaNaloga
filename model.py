c8e5a41fb0fd21bd698a51deff300f6e61bcaea3
Vid Treven <treven.vid@gmail.com>
2022-07-26 20:26:29 +0200

    kocno najdel

--- model.py
+++ model.py
@@ -1,45 +1,68 @@
 from datetime import date
+import json
 
 
 class Stanje:
-    def __init__(self):
-        self.spiski = []
-        self.aktualni_spisek = None
+    def __init__(self, kategorije):
+        self.kategorije = kategorije
 
-    def dodaj_spisek(self, spisek):
-        self.spiski.append(spisek)
-        if not self.aktualni_spisek:
-            self.aktualni_spisek = spisek
+    def dodaj_kategorijo(self, kategorija):
+        self.kategorije.append(kategorija)
+        return len(self.kategorije) - 1
 
-    def pobrisi_spisek(self, spisek):
-        self.spiski.remove(spisek)
+    def preveri_podatke_nove_kategorije(self, nova_kategorija):
+        for kategorija in self.kategorije:
+            if kategorija.ime == nova_kategorija.ime:
+                return {"ime": "Kategorija s tem imenom že obstaja"}
 
-    def zamenjaj_spisek(self, spisek):
-        self.aktualni_spisek = spisek
+    def v_slovar(self):
+        return {
+            "kategorije": [kategorija.v_slovar() for kategorija in self.kategorije],
+        }
 
-    def dodaj_opravilo(self, opravilo):
-        self.aktualni_spisek.dodaj_opravilo(opravilo)
+    @staticmethod
+    def iz_slovarja(slovar):
+        stanje = Stanje(
+            [
+                Kategorija.iz_slovarja(sl_kategorije)
+                for sl_kategorije in slovar["kategorije"]
+            ]
+        )
+        return stanje
 
-    def pobrisi_opravilo(self, opravilo):
-        self.aktualni_spisek.pobrisi_opravilo(opravilo)
+    def shrani_v_datoteko(self, ime_datoteke):
+        with open(ime_datoteke, "w") as dat:
+            slovar = self.v_slovar()
+            json.dump(slovar, dat, indent=4, ensure_ascii=False)
 
-class Spisek:
-    def __init__(self, ime):
+    @staticmethod
+    def preberi_iz_datoteke(ime_datoteke):
+        with open(ime_datoteke) as dat:
+            slovar = json.load(dat)
+            return Stanje.iz_slovarja(slovar)
+
+
+class Kategorija:
+    def __init__(self, ime, opravila):
         self.ime = ime
-        self.opravila = []
+        self.opravila = opravila
+
+    def stevilo_neopravljenih(self):
+        neopravljena = 0
+        for opravilo in self.opravila:
+            if not opravilo.opravljeno:
+                neopravljena += 1
+        return neopravljena
 
     def dodaj_opravilo(self, opravilo):
         self.opravila.append(opravilo)
 
     def stevilo_zamujenih(self):
-        stevilo = 0
+        zamujena = 0
         for opravilo in self.opravila:
             if opravilo.zamuja():
-                stevilo += 1
-        return stevilo
-
-    def stevilo_vseh(self):
-        return len(self.opravila)
+                zamujena += 1
+        return zamujena
 
     def v_slovar(self):
         return {
@@ -47,10 +70,16 @@ class Spisek:
             "opravila": [opravilo.v_slovar() for opravilo in self.opravila],
         }
 
+    @staticmethod
+    def iz_slovarja(slovar):
+        return Kategorija(
+            slovar["ime"],
+            [Opravilo.iz_slovarja(sl_opravila) for sl_opravila in slovar["opravila"]],
+        )
+
 
 class Opravilo:
-    def __init__(self, ime, opis, rok, opravljeno=False):
-        self.ime = ime
+    def __init__(self, opis, rok, opravljeno=False):
         self.opis = opis
         self.rok = rok
         self.opravljeno = opravljeno
@@ -61,11 +90,18 @@ class Opravilo:
     def zamuja(self):
         rok_pretekel = self.rok and self.rok < date.today()
         return not self.opravljeno and rok_pretekel
-        
+
     def v_slovar(self):
         return {
-            "ime": self.ime,
             "opis": self.opis,
-            "rok": date.isoformat(self.rok) if self.rok else None,
+            "rok": self.rok.isoformat() if self.rok else None,
             "opravljeno": self.opravljeno,
         }
+
+    @staticmethod
+    def iz_slovarja(slovar):
+        return Opravilo(
+            slovar["opis"],
+            date.fromisoformat(slovar["rok"]) if slovar["rok"] else None,
+            slovar["opravljeno"],
+        )
